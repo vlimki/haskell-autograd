a6a8e3fa2b84e75b73a14e733a840d31ee6fae4f
vlimki <vlimki@proton.me>
2024-08-08 20:07:45 +0300

    remove value from leaf

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,7 +14,7 @@ data Value
   deriving (Show, Eq)
 
 data Calculated
-  = Leaf Value Double
+  = Leaf Double String
   | MulBranch Calculated Calculated Double String
   | AddBranch Calculated Calculated Double String
   deriving (Show, Eq)
@@ -26,25 +26,21 @@ add :: Value -> Value -> String -> Value
 add = Add
 
 extractId :: Calculated -> String
-extractId (Leaf (Value _ id') _) = id'
+extractId (Leaf _ id') = id'
 extractId (AddBranch _ _ _ id') = id'
 extractId (MulBranch _ _ _ id') = id'
-extractId _ = error "Impossible"
 
 extractCalculation :: Calculated -> Double
-extractCalculation (Leaf (Value _ _) x) = x
+extractCalculation (Leaf x _) = x
 extractCalculation (AddBranch _ _ x _) = x
 extractCalculation (MulBranch _ _ x _) = x
-extractCalculation _ = error "Impossible"
 
 idMultiplier :: Calculated -> String -> Double
 idMultiplier v i = if extractId v == i then 1 else 0
--- In MulBranch, if id y == id, then multiply by x, otherwise multiply by y if the id is one of them
--- 3 * (grad x) or 2 * (grad y)
 
 feedForward :: Value -> Calculated
 feedForward v = case v of
-  Value x idv -> Leaf (Value x idv) x
+  Value x idv -> Leaf x idv
   Add (Value x idx) (Value y idy) i -> AddBranch (feedForward $ Value x idx) (feedForward $ Value y idy) (x + y) i
   Mul (Value x idx) (Value y idy) i -> MulBranch (feedForward $ Value x idx) (feedForward $ Value y idy) (x * y) i
   Add x y i -> AddBranch (feedForward x) (feedForward y) (calculate x + calculate y) i
