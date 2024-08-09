13b30ea8170cf80196d2c4a52840c6b57a846a9b
vlimki <vlimki@proton.me>
2024-08-09 16:27:14 +0300

    Outer type + grad function now works for sigmoid and ReLU

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,6 +9,12 @@ module Lib
   , (!+)
   ) where
 
+data Outer = Outer {
+  _inner :: [Calculated],
+  _calculation :: Double,
+  _id :: String,
+  _grad :: Double
+} deriving (Show, Eq)
 -- Value is essentially just a convenient API to the engine. The calculations are done with the `Calculated` type instead.
 data Value
   = Value Double String
@@ -19,11 +25,11 @@ data Value
   deriving (Show, Eq)
 
 data Calculated
-  = Leaf Double String
-  | IReLU Calculated Double String
-  | ISigmoid Calculated Double String
-  | IMul Calculated Calculated Double String
-  | IAdd Calculated Calculated Double String
+  = Leaf Outer 
+  | IReLU Outer
+  | ISigmoid Outer
+  | IMul Outer
+  | IAdd Outer
   deriving (Show, Eq)
 
 mul :: Value -> Value -> String -> Value
@@ -51,23 +57,23 @@ idMultiplier v i = if extractId v == i then 1 else 0
 
 feedForward :: Value -> Calculated
 feedForward v = case v of
-  Value x idv -> Leaf x idv
-  Add (Value x idx) (Value y idy) i -> IAdd (feedForward $ Value x idx) (feedForward $ Value y idy) (x + y) i
-  Mul (Value x idx) (Value y idy) i -> IMul (feedForward $ Value x idx) (feedForward $ Value y idy) (x * y) i
-  ReLU (Value x idx) i -> IReLU (feedForward $ Value x idx) (max x 0) i
-  Sigmoid (Value x idx) i -> ISigmoid (feedForward $ Value x idx) (sigmoid x) i
-  ReLU x i -> IReLU (feedForward x) (max (calculate x) 0) i
-  Sigmoid x i -> ISigmoid (feedForward x) (sigmoid (calculate x)) i
-  Add x y i -> IAdd (feedForward x) (feedForward y) (calculate x + calculate y) i
-  Mul x y i -> IMul (feedForward x) (feedForward y) (calculate x * calculate y) i
+  Value x idv -> Leaf (Outer {_inner=[], _calculation=x, _id=idv, _grad=0})
+  Add (Value x idx) (Value y idy) i -> IAdd (Outer [feedForward $ Value x idx, feedForward $ Value y idy] (x + y) i 0)
+  Mul (Value x idx) (Value y idy) i -> IMul (Outer [feedForward $ Value x idx, feedForward $ Value y idy] (x * y) i 0)
+  ReLU (Value x idx) i -> IReLU (Outer [feedForward $ Value x idx] (max x 0) i 0)
+  Sigmoid (Value x idx) i -> ISigmoid (Outer [feedForward $ Value x idx] (sigmoid x) i 0)
+  ReLU x i -> IReLU (Outer [feedForward x] (max (calculate x) 0) i 0)
+  Sigmoid x i -> ISigmoid (Outer [feedForward x] (sigmoid $ calculate x) i 0)
+  Add x y i -> IAdd (Outer [feedForward x, feedForward y] (calculate x + calculate y) i 0)
+  Mul x y i -> IMul (Outer [feedForward x, feedForward y] (calculate x * calculate y) i 0)
 
 grad :: String -> Calculated -> Double
 grad id' v = case v of
-  IReLU x _ i -> if i == id' then (if extractCalculation x > 0 then 1 else 0) else grad id' x
-  ISigmoid x _ i -> if i == id' then sigmoid' $ extractCalculation x else grad id' x
-  IAdd x y _ i -> if i == id' then 1 else grad id' x + grad id' y
-  IMul x y _ i -> if i == id' then 1 else (extractCalculation y * grad id' x) + (extractCalculation x * grad id' y)
-  x@(Leaf _ _) -> idMultiplier x id'
+  IReLU (Outer x _ i _) -> if i == id' then 1 else (if extractCalculation (head x) > 0 then 1 else 0) * grad id' (head x)
+  ISigmoid (Outer x _ i _) -> if i == id' then 1 else sigmoid' $ grad id' (head x)
+  IAdd (Outer items _ i _) -> if i == id' then 1 else grad id' (head items) + grad id' (head $ tail items)
+  IMul (Outer items _ i _) -> if i == id' then 1 else (extractCalculation (head $ tail items) * grad id' (head items)) + (extractCalculation (head items) * grad id' (head $ tail items))
+  x@(Leaf _) -> idMultiplier x id'
 
 calculate :: Value -> Double
 calculate v = case v of
@@ -84,15 +90,15 @@ calculate v = case v of
 -- AddBranch x y
 
 extractId :: Calculated -> String
-extractId (Leaf _ id') = id'
-extractId (IReLU _ _ id') = id'
-extractId (ISigmoid _ _ id') = id'
-extractId (IAdd _ _ _ id') = id'
-extractId (IMul _ _ _ id') = id'
+extractId (Leaf (Outer _ _ id' _)) = id'
+extractId (IReLU (Outer _ _ id' _)) = id'
+extractId (ISigmoid (Outer _ _ id' _)) = id'
+extractId (IAdd (Outer _ _ id' _)) = id'
+extractId (IMul (Outer _ _ id' _)) = id'
 
 extractCalculation :: Calculated -> Double
-extractCalculation (Leaf x _) = x
-extractCalculation (IReLU _ x _) = x
-extractCalculation (ISigmoid _ x _) = x
-extractCalculation (IAdd _ _ x _) = x
-extractCalculation (IMul _ _ x _) = x
+extractCalculation (Leaf (Outer _ calc _ _)) = calc
+extractCalculation (IReLU (Outer _ calc _ _)) = calc
+extractCalculation (ISigmoid (Outer _ calc _ _)) = calc
+extractCalculation (IAdd (Outer _ calc _ _)) = calc
+extractCalculation (IMul (Outer _ calc _ _)) = calc
